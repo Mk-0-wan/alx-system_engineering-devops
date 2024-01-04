4a5e3f2ec55d79ee86261b0273515c06941d12be
root <root@Media>
2024-01-05 02:49:10 +0300

    collecting data from text messages

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,3 @@
 #!/usr/bin/env ruby
-# matching the exact work given
-# puts ARGV[0].scan(/((?<=[from:])\w+|\d+)\-1,((?<=[to:])\w+|\d+)\-2,((?<=[flags:])\d+)\-3/).join
-# puts ARGV[0].scan(/from:(\w+|\d+)-1,to:(\w+|\d+)-2,flags:(\d+)-3/).join
-puts ARGV[0].scan(/\[from:(\w+|\+?\d+)\] \[to:(\+?\d+)\] \[flags:(\S+:\S+:\S+:\S+:\S+)\]/).join(",")
+# matching the exact work given grouping each pattern individually then collecting each one expected behaviour
+puts ARGV[0].scan(/\[from:(\w+|\+?\d+)\] \[to:(\+?\d+)\] \[flags:(.*?)\]/).join(",")
