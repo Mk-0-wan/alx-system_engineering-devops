3664eac31e07080d24b972d3be95040eea9d9288
mk <victormurithi2020@gmail.com>
2024-02-27 07:03:58 +0300

    finally

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,6 @@
 # 100_puppet_ssh_config
 
-$ip_address = '3.85.196.185'
-$mode = '0644'
-$path = '/etc/ssh/ssh_config'
-$content = "Host $ip_address\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no\n"
-
-file { $path :
-	ensure   => present,
-	content  => $content,
-	owner    => 'root',
-	group    => 'root',
-	mode     => $mode,
-
+exec {'Puppet Configuration':
+  path    => '/usr/bin:/bin',
+  command => 'echo -e "Host 3.85.196.185\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no" >> /etc/ssh/ssh_config',
 }
