56c011b53feb0bdeaf5c675f47e1602734d4aec4
mk <victormurithi2020@gmail.com>
2024-03-07 16:14:26 +0300

    Adding new header content with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,26 @@
+# using puppet to set up the new header content
+package {'nginx':
+	ensure => installed
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World'
+}
+
+file {'/var/www/html/404.html':
+	content => 'Ceci n'est pas une page'
+}
+
+exec {'add_custom_header':
+	command => 'sed -i "23i \\\tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	path    => '/bin:/usr/bin/',
+	onlyif  => 'test ! -z "$(grep -E "^\s*add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default)"',
+	notify  => Service['nginx'],
+}
+
+service {'nginx':
+	ensure    => running,
+	enable    => true,
+	require   => Package['nginx'],
+	subscribe => Exec['add_custom_header'],
+}
