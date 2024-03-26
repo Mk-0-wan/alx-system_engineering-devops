9554a561bc6aed913d5c01909e695eb1975285b5
mk <victormurithi2020@gmail.com>
2024-03-26 05:50:21 +0300

    added

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+getting information about an employee by ID and returns their TODO progress
+'''
+import csv
+import os
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    emp = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                       .format(sys.argv[1]))
+    name = emp.json().get('username')
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = tasks.json()
+    complete = 0
+    fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+    titles = []
+    total = 0
+    filename = sys.argv[1] + ".csv"
+    with open(filename, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL, quotechar='"')
+        for task in tasks:
+            if task['userId'] == int(sys.argv[1]):
+                task['name'] = name
+                writer.writerow([task['userId'], task['name'],
+                                 task['completed'], task['title']])
